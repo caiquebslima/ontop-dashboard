be3d3e0e0de571c8527d5ae4aa559a5f576870e6
caiquebslima <caiquebslima@gmail.com>
2022-06-24 23:57:03 -0300

    feat: started creation of dashboard feat: user menu, dashboard and header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
+import { MatMenuModule } from '@angular/material/menu'
+import { UserMenuComponent } from './components/user-menu/user-menu.component'
+import { MatInputModule } from '@angular/material/input'
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, DashboardComponent],
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        DashboardComponent,
+        UserMenuComponent,
+    ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
         MatToolbarModule,
         MatIconModule,
         MatButtonModule,
+        MatMenuModule,
+        MatInputModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
---
+++ src/app/components/user-menu/user-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+    selector: 'app-user-menu',
+    templateUrl: './user-menu.component.html',
+})
+export class UserMenuComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
